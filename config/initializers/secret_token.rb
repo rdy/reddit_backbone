91397c64aa380e04ae461c3801224dd28ba18247
Ryan Dy <ryan.dy@gmail.com>
2011-11-02 18:58:39 -0700

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedditBackbone::Application.config.secret_token = '500c23a07ad7a4e6212962d8c580ba3791259847681302ad5cf3f72098c70121a3767a4cc50a2a472ae59683b53fccdf8437897a92863b5d4ca4d7458450e36f'
